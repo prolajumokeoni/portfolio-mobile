a71a12bb366f5857e464a0b069767649173b9f2b
prolajumokeoni <prolajumokeoni@gmail.com>
2022-02-04 18:16:59 +0100

    Add click event for close button and style close button

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const menuBars = document.querySelector('.menu-bars');
+const closeMenu = document.querySelector('.close');
 const menuNav = document.querySelector('.desktop-nav');
+const navLinks = document.querySelectorAll('.nav-link');
 
-menuBars.addEventListener('click', () => {
-  menuNav.classList.add('visible');
-});
+const closeMobileNav = () => {
+  menuNav.classList.remove('visible');
+};
+
+window.onload = () => {
+  menuBars.addEventListener('click', () => {
+    menuNav.classList.add('visible');
+  });
+  closeMenu.addEventListener('click', () => {
+    closeMobileNav();
+  });
+  Array.from(navLinks).map((link) => link.addEventListener('click', () => {
+    closeMobileNav();
+  }));
+};
